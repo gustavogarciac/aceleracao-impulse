015bb85289a3220ee1088c5440a6ffdcbadfdda5
gustavo <gustavogarciaci1@gmail.com>
2024-08-27 19:22:46 -0300

    feat: next steps section responsivity

--- src/app/_components/sections/next-steps-section.tsx
+++ src/app/_components/sections/next-steps-section.tsx
@@ -5,18 +5,20 @@ import { nextStepsCards } from '@/consts/cards'
 
 export const NextStepsSection = () => {
   return (
-    <section className="flex flex-col items-center justify-center gap-12 pt-10 px-6 pb-20">
+    <section className="flex flex-col items-center justify-center gap-12 pt-10 px-6 pb-20 ">
       <h2 className="subtitle text-center font-bold text-2xl text-theme-blue-default">
         Qual o próximo passo?
       </h2>
 
-      {nextStepsCards.map((card, index) => (
-        <Card key={card.title} variant={'secondary'}>
-          <CardIndex index={index + 1} variant={'secondary'} />
-          <CardTitle variant={'secondary'}>{card.title}</CardTitle>
-          <CardContent>{card.content}</CardContent>
-        </Card>
-      ))}
+      <div className="grid grid-cols-1 gap-12 md:grid-cols-2 md:gap-2 lg:max-w-3xl mx-auto">
+        {nextStepsCards.map((card, index) => (
+          <Card key={card.title} variant={'secondary'}>
+            <CardIndex index={index + 1} variant={'secondary'} />
+            <CardTitle variant={'secondary'}>{card.title}</CardTitle>
+            <CardContent>{card.content}</CardContent>
+          </Card>
+        ))}
+      </div>
     </section>
   )
 }
