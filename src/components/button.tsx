12c38a6f8afeded7c6b628fc4a11eb712db04a8e
gustavo <gustavogarciaci1@gmail.com>
2024-08-27 17:22:04 -0300

    style: changed button styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,20 @@
 import React, { ButtonHTMLAttributes } from 'react'
 
+import { cn } from '@/lib/utils'
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
+  className?: string
 }
 
-export const Button = ({ children }: ButtonProps) => {
+export const Button = ({ children, className }: ButtonProps) => {
   return (
-    <button className="flex items-center justify-center bg-theme-green-default hover:bg-theme-green-dark transition-colors duration-150 ease-in-out cursor-pointer text-sm md:text-base lg:text-lg font-semibold">
+    <button
+      className={cn(
+        'flex items-center justify-center bg-theme-green-default hover:bg-theme-green-dark transition-colors duration-150 ease-in-out cursor-pointer text-sm md:text-base lg:text-lg font-semibold rounded-full px-6 py-4 w-full',
+        className,
+      )}
+    >
       {children}
     </button>
   )
