d48b20c8ae2205c081fb25cec55b2b4af7bf270e
gustavo <gustavogarciaci1@gmail.com>
2024-08-27 18:54:16 -0300

    feat: hero responsivity

--- src/app/_components/sections/hero.tsx
+++ src/app/_components/sections/hero.tsx
@@ -5,7 +5,7 @@ import { ProgramTitle } from '../program-title'
 
 export const Hero = () => {
   return (
-    <section className="flex flex-col items-center justify-center gap-12 pt-32 px-6 pb-10">
+    <section className="flex flex-col items-center justify-center gap-12 pt-32 lg:pt-28 px-6 pb-10">
       <ProgramTitle />
 
       <h2 className="subtitle font-bold text-center">
@@ -14,7 +14,8 @@ export const Hero = () => {
 
       <p className="text-center text-body">
         Se posicione para conquistar uma carreira acima dos{' '}
-        <span className="text-theme-blue-default">R$ 10.000</span> por mês
+        <span className="text-theme-blue-default font-semibold">R$ 10.000</span>{' '}
+        por mês
       </p>
 
       <ul className="flex flex-col gap-2">
